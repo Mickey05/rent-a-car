814c5bb61878e17a5945bd430f6fadd3daf658df
MiqueasGarcía <miqueas05@users.noreply.github.com>
2020-05-13 23:59:18 -0400

    Vue added

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+window.Vue = Vue
+
+Vue.component('index', () =>
+  import('./components/Index.vue'))
+
+const app = new Vue({
+    el: '#app'
+});
